a9d97fdc6419a7c9ba2297bfc00ad7a75718bd47
dori.lahav <dori.lahav@gmail.com>
2022-11-16 13:10:19 +0200

    wip (#6)
    Co-authored-by: Yasmin <y2003s@gmail.com>

--- src/routes/current-ticket/ticket-details-sidebar/TicketDetailsSidebar.jsx
+++ src/routes/current-ticket/ticket-details-sidebar/TicketDetailsSidebar.jsx
@@ -4,8 +4,8 @@ import { TicketDetails } from './TicketDetails';
 import { TicketFileList } from './TicketFileList';
 import { TicketTroubleshooters } from './TicketTroubleshooters';
 
-export const TicketDetailsSidebar = ({ticket, onAddTeam}) => (
-  <Paper sx={{width: 375, px: 1, pb: 2, overflow: 'auto', height: '100%'}}>
+export const TicketDetailsSidebar = ({width = 375, ticket, onAddTeam}) => (
+  <Paper sx={{width, px: 1, pb: 2, overflow: 'auto', height: '100%'}}>
     <Header>מטפלים בתקלה</Header>
     <TicketTroubleshooters {...ticket.troubleshooters} onCreateTeam={onAddTeam}/>
     <Header>פרטי התקלה</Header>
