823dc21e7dab9a91d9475839e588b46c32cab62b
ofirComputer <ofirComputer@example.com>
2022-11-16 13:38:10 +0200

    feat(App): change scrollbar style

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { AppLayout } from './layout';
-import { ThemeProvider } from './providers';
-import { Routes } from './routes';
+import { AppLayout } from "./layout";
+import { ThemeProvider } from "./providers";
+import { Routes } from "./routes";
+import "./styles/scrollbar.css";
 
 function App() {
   return (
     <ThemeProvider>
       <AppLayout>
-        <Routes/>
+        <Routes />
       </AppLayout>
     </ThemeProvider>
-  )
+  );
 }
 
 export default App;
